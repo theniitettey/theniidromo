8975c344c2de7e0bddd74d7fa53b129a6f0d49f1
Nii Dromo <michaelperryt97@gmail.com>
2026-05-14 17:49:54 +0000

    feat: add Cmd+K search modal

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -7,5 +7,6 @@ import { DjQueueWidget } from "./DjQueueWidget";
 import { ViewTracker } from "./ViewTracker";
 import { FloatingNowPlaying } from "./FloatingNowPlaying";
 import { ReadingProgress } from "./ReadingProgress";
+import { SearchModal, useSearchModal } from "./SearchModal";
 
-export { MotionHeader, MotionFooter, MotionDiv, Spoiler, SpotifyCard, SignaturePad, NowPlaying, DjQueueWidget, ViewTracker, FloatingNowPlaying, ReadingProgress };
+export { MotionHeader, MotionFooter, MotionDiv, Spoiler, SpotifyCard, SignaturePad, NowPlaying, DjQueueWidget, ViewTracker, FloatingNowPlaying, ReadingProgress, SearchModal, useSearchModal };
