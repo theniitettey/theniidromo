252fd033b011f7ef901350b3ac68bacf02866d83
Nii Dromo <michaelperryt97@gmail.com>
2026-05-14 17:27:35 +0000

    export GoogleOneTap from ui index

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -4,5 +4,6 @@ import { SpotifyCard } from "./SpotifyCard";
 import { SignaturePad } from "./SignaturePad";
 import { NowPlaying } from "./NowPlaying";
 import { DjQueueWidget } from "./DjQueueWidget";
+import { GoogleOneTap } from "./GoogleOneTap";
 
-export { MotionHeader, MotionFooter, MotionDiv, Spoiler, SpotifyCard, SignaturePad, NowPlaying, DjQueueWidget };
+export { MotionHeader, MotionFooter, MotionDiv, Spoiler, SpotifyCard, SignaturePad, NowPlaying, DjQueueWidget, GoogleOneTap };
