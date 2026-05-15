cad93bfa24a9a15ef519e5b7d8868e3eb5502530
Nii Dromo <michaelperryt97@gmail.com>
2026-05-15 16:36:18 +0000

    add central person data file

---
+++ src/data/person.ts
@@ -0,0 +1,22 @@
+export const person = {
+  name: "Michael Perry Nii Dromo Tettey",
+  shortName: "Nii Dromo",
+  title: "Software Engineer",
+  email: "michaelperryt97@gmail.com",
+
+  social: {
+    twitter:   "https://x.com/theniitettey",
+    linkedin:  "https://linkedin.com/in/theniitettey",
+    github:    "https://github.com/michaelperryjnr",
+    youtube:   "https://youtube.com/@michaelperryjnr",
+    twitterHandle: "@theniitettey",
+    youtubeHandle: "@michaelperryjnr",
+    githubHandle:  "michaelperryjnr",
+  },
+
+  projects: {
+    qz:         "https://qz.bflabs.tech",
+    eventflick: "https://eventflick.vercel.app",
+    themefolio: "https://github.com/michaelperryjnr/themefolio",
+  },
+};
